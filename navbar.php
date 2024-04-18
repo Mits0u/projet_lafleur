945a94ebcb4c7e829b3a3d24400b24911a6c2b69
BoppA <albopp@stpbb.org>
2024-04-18 08:54:21 +0200

    produits

--- navbar.php
+++ navbar.php
@@ -2,8 +2,8 @@
     <div class="container mx-auto flex justify-between items-center">
         <a class="text-lg font-bold text-gray-800" href="#">LaFleur</a>
         <div class="flex space-x-4">
-            <a class="text-gray-600 hover:text-gray-800" href="#">Accueil</a>
-            <a class="text-gray-600 hover:text-gray-800" href="#">Produits</a>
+            <a class="text-gray-600 hover:text-gray-800" href="index.php">Accueil</a>
+            <a class="text-gray-600 hover:text-gray-800" href="produits.php">Produits</a>
             <a class="text-gray-600 hover:text-gray-800" href="#">À propos</a>
             <a class="text-gray-600 hover:text-gray-800" href="#">Contact</a>
             <a class="text-gray-600 hover:text-gray-800" href="#"><i class="fas fa-shopping-cart text-gray-800"></i></a>
